c6b4555001e5c60c36fdfd1611792eaf43651d2b
KatiePee <piele.katie@gmail.com>
2023-08-22 18:26:05 -0700

    tailwind installed and tested, mock homepage up

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,22 +3,20 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reloa.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+    <div className='flex-col justify-center align-center border-4 border-gray-200 p-4 rounded-lg space-y-6'>
+      <h1 className='text-gray-600 text-2xl font-semibold border-b-2 border-gray-200 text-center'>Riddle Me This....</h1>
+      <div className='p-8 bg-gray-200 text-gray-600 rounded-lg'>If there's a Bee in my hand<br />What's in my eye?...</div>
+      <div className='flex'>
+
+        <button className='flex-1 hover:bg-gray-200 rounded-lg p-4'>Log In</button>
+        <button className='flex-1 hover:bg-gray-200 rounded-lg p-4'>Sign Up</button>
+      </div>
+
+
     </div>
+
+
   );
 }
 
